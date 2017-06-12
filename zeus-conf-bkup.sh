15a4a9e1c55c3ee114fcdcf7e263e31d89d67396
Scott Idler <scott.a.idler@gmail.com>
2017-06-12 12:55:21 -0700

    initial add of zeus-conf-bkup.sh script to be used in a cron to backup zeus configs

---
+++ zeus-conf-bkup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This is meant to be used as a shell script invoked in a cron for backing up
+# the current state of the conf/ folder in zeus' install path.
+
+#provide correct path to override the following two positional arguments
+CONF=${1:-~/zeus-conf/}
+BKUP=${2:-~/zeus-bkup/}
+
+if ! hash git 2> /dev/null; then
+    echo "git not installed!"
+    exit 1
+fi
+if ! hash rsync 2> /dev/null; then
+    echo "rsync not installed!"
+    exit 1
+fi
+
+mkdir -p $BKUP
+rsync -rav --delete --exclude=.git/ $CONF $BKUP
+cd $BKUP
+if [ "true" != "`git rev-parse --is-inside-work-tree 2> /dev/null`" ]; then
+    git init
+fi
+git add .
+MSG="`git status --short`"
+if [ "$MSG" ]; then
+    git commit -m "$MSG"
+fi
